0dff89177f86157945bf5f697df3144f2f23181e
Nicolas <nicolas.vigou@gmail.com>
2026-04-29 17:11:33 +0200

    db.Database.Migrate();

--- Meran.Back/Program.cs
+++ Meran.Back/Program.cs
@@ -122,4 +122,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
